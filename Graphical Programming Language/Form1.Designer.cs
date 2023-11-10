6ba503b0842e9df79c8e8c91e4747866aa1a6210
Flavio <flavio.pires.1999@gmail.com>
2023-11-10 01:48:06 +0000

    Added the red dot into the panel

--- Graphical Programming Language/Form1.Designer.cs
+++ Graphical Programming Language/Form1.Designer.cs
@@ -28,88 +28,98 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
-            this.button5 = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.LoadButton = new System.Windows.Forms.Button();
+            this.SaveButton = new System.Windows.Forms.Button();
+            this.RunButton1 = new System.Windows.Forms.Button();
+            this.SyntaxButton = new System.Windows.Forms.Button();
+            this.RunButton2 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
-            // button1
+            // LoadButton
             // 
-            this.button1.Location = new System.Drawing.Point(12, 50);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.LoadButton.Location = new System.Drawing.Point(12, 50);
+            this.LoadButton.Name = "LoadButton";
+            this.LoadButton.Size = new System.Drawing.Size(75, 23);
+            this.LoadButton.TabIndex = 0;
+            this.LoadButton.Text = "Load";
+            this.LoadButton.UseVisualStyleBackColor = true;
             // 
-            // button2
+            // SaveButton
             // 
-            this.button2.Location = new System.Drawing.Point(125, 50);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
+            this.SaveButton.Location = new System.Drawing.Point(125, 50);
+            this.SaveButton.Name = "SaveButton";
+            this.SaveButton.Size = new System.Drawing.Size(75, 23);
+            this.SaveButton.TabIndex = 1;
+            this.SaveButton.Text = "Save";
+            this.SaveButton.UseVisualStyleBackColor = true;
             // 
-            // button3
+            // RunButton1
             // 
-            this.button3.Location = new System.Drawing.Point(12, 450);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
-            this.button3.TabIndex = 2;
-            this.button3.Text = "button3";
-            this.button3.UseVisualStyleBackColor = true;
+            this.RunButton1.Location = new System.Drawing.Point(12, 450);
+            this.RunButton1.Name = "RunButton1";
+            this.RunButton1.Size = new System.Drawing.Size(75, 23);
+            this.RunButton1.TabIndex = 2;
+            this.RunButton1.Text = "Run";
+            this.RunButton1.UseVisualStyleBackColor = true;
             // 
-            // button4
+            // SyntaxButton
             // 
-            this.button4.Location = new System.Drawing.Point(125, 450);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(75, 23);
-            this.button4.TabIndex = 3;
-            this.button4.Text = "button4";
-            this.button4.UseVisualStyleBackColor = true;
+            this.SyntaxButton.Location = new System.Drawing.Point(125, 450);
+            this.SyntaxButton.Name = "SyntaxButton";
+            this.SyntaxButton.Size = new System.Drawing.Size(75, 23);
+            this.SyntaxButton.TabIndex = 3;
+            this.SyntaxButton.Text = "Syntax";
+            this.SyntaxButton.UseVisualStyleBackColor = true;
             // 
-            // button5
+            // RunButton2
             // 
-            this.button5.Location = new System.Drawing.Point(12, 544);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(75, 23);
-            this.button5.TabIndex = 4;
-            this.button5.Text = "button5";
-            this.button5.UseVisualStyleBackColor = true;
+            this.RunButton2.Location = new System.Drawing.Point(12, 544);
+            this.RunButton2.Name = "RunButton2";
+            this.RunButton2.Size = new System.Drawing.Size(75, 23);
+            this.RunButton2.TabIndex = 4;
+            this.RunButton2.Text = "Run";
+            this.RunButton2.UseVisualStyleBackColor = true;
             // 
-            // textBox1
+            // textBox2
             // 
-            this.textBox1.Location = new System.Drawing.Point(12, 88);
-            this.textBox1.Multiline = true;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(331, 356);
-            this.textBox1.TabIndex = 5;
+            this.textBox2.Location = new System.Drawing.Point(12, 88);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(331, 356);
+            this.textBox2.TabIndex = 5;
             // 
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.SystemColors.ControlDark;
-            this.panel1.Location = new System.Drawing.Point(507, 88);
+            this.panel1.Location = new System.Drawing.Point(508, 50);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(473, 356);
+            this.panel1.Size = new System.Drawing.Size(496, 517);
             this.panel1.TabIndex = 6;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 498);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(330, 20);
+            this.textBox1.TabIndex = 7;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1052, 625);
-            this.Controls.Add(this.panel1);
             this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.button5);
-            this.Controls.Add(this.button4);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.RunButton2);
+            this.Controls.Add(this.SyntaxButton);
+            this.Controls.Add(this.RunButton1);
+            this.Controls.Add(this.SaveButton);
+            this.Controls.Add(this.LoadButton);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -119,12 +129,13 @@
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button4;
-        private System.Windows.Forms.Button button5;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button LoadButton;
+        private System.Windows.Forms.Button SaveButton;
+        private System.Windows.Forms.Button RunButton1;
+        private System.Windows.Forms.Button SyntaxButton;
+        private System.Windows.Forms.Button RunButton2;
+        private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
--- Graphical Programming Language/Form1.cs
+++ Graphical Programming Language/Form1.cs
@@ -12,9 +12,30 @@ namespace Graphical_Programming_Language
 {
     public partial class Form1 : Form
     {
+        public int dotSize = 20;
+        public int centerX = 10;
+        public int centerY = 10;
+        public Color dotColor = Color.Red;
+        private Brush dotBrush;
+
         public Form1()
         {
             InitializeComponent();
+            panel1.Invalidate();
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            using (Graphics g = e.Graphics)
+            {
+                // Draw the dot on the panel using class-level fields
+                dotBrush = new SolidBrush(dotColor);
+
+                // Draw the dot on the panel at the current position
+                g.FillEllipse(dotBrush, centerX - dotSize / 2, centerY - dotSize / 2, dotSize, dotSize);
+
+            }
         }
     }
 }
